099060019b12db5677f211d405cfd41a13f5bd1a
nouhaila <nouhaila@nouha>
2020-11-02 17:06:52 +0000

    Merge origin/master

---
+++ JavaApplication3/src/javaapplication3/Client.java
@@ -0,0 +1,16 @@
+package javaapplication3;
+public class Client { 
+
+                public static void main(String[] args) {
+		Fichier child1 = new Fichier(100L);
+		Fichier child2 = new Fichier(200L);
+
+		Dossier root= new Dossier(100L);
+		root.addFile(child1);
+		root.addFile(child2);
+
+
+		System.out.println(child1.getTaille());      // output : 100
+		System.out.println(root.getTaille());    // output : 300
+	}   
+    }
